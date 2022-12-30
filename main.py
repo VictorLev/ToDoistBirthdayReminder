f4016f644a8f96eaafda1b029486fc7a743599e7
Victor <levesque.vic@gmail.com>
2022-12-30 17:16:45 +1100

    t

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from datetime import datetime
-import pytz
+from datetime import timedelta,date
 from addtask import send_task
 from ListofBirthdays import BirthdayDictionary  
 
-#test git ignore
-
-def lambda_handler(event, context):
-
-    MelTimeZone= pytz.timezone('Australia/Melbourne')
-    today = datetime.now(MelTimeZone)
+def main():
+    today = date.today()+ timedelta(days=1)
     TheDate = today.strftime('%m-%d')
     for index, Birthdate in enumerate(BirthdayDictionary[1]):
         if TheDate == Birthdate:
             send_task(BirthdayDictionary[0][index])
             print('its '+ BirthdayDictionary[0][index]+ ' Birthday')
     
+if __name__ == "__main__":
+    main()
